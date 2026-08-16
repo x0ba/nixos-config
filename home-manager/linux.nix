1abd22d9df89af468f43a116e96e1f013ff92776
Daniel Xu <hi@danielx.me>
2026-08-16 01:33:35 -0700

    exec herdr automatically

--- home-manager/linux.nix
+++ home-manager/linux.nix
@@ -1,6 +1,22 @@
-{ ... }:
+{ lib, pkgs, ... }:
+let
+  herdr = lib.getExe pkgs.herdr;
+in
 {
   targets.genericLinux.enable = true;
 
   services.ssh-agent.enable = true;
+
+  # Interactive SSH only. HERDR_ENV is set inside panes, so this does not nest.
+  programs.fish.interactiveShellInit = lib.mkBefore ''
+    if set -q SSH_CONNECTION; and not set -q HERDR_ENV
+      exec ${herdr}
+    end
+  '';
+
+  programs.zsh.initExtraFirst = lib.mkBefore ''
+    if [[ -n "$SSH_CONNECTION" && -z "$HERDR_ENV" ]]; then
+      exec ${herdr}
+    fi
+  '';
 }
